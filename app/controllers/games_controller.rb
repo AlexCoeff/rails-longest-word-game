735cf0b367d1d9947cfab78976a4b8b96878ca30
Alexandre Coëffet <alexandre.coeffet@hec.ca>
2020-11-10 11:19:55 -0500

    day 1 rails

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    array = word.chars
+    array.all? do |letter|
+      array.count(letter) <= letters.count(letter)
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
